59661290b261bccf1a3611bafffe5d5ae742fdbc
coazz <aco94@hotmail.com>
2022-11-19 21:11:30 +0100

    chnages

--- main.js
+++ main.js
@@ -30,7 +30,10 @@ const gameBoard = (() => {
       for(let i = 0; i < board.length; i++){
         board[i] = "";
      }
+     // Every time we reset the board we update the display
+      displayControl.updateBoard()
     }
+
     const printBoard = () => console.log(board)
 
     return {changeBoard, boardLenght, boardSymbol, resetBoard, printBoard}
@@ -42,23 +45,35 @@ const displayControl = (() => {
     // Get all the fields from the html
     let fields = document.querySelectorAll("[id^=square_]");
     let player_text = document.getElementById("playingtext");
+    let reset_button = document.getElementById("r_button");
 
+    // if dom loaded change player text
+    document.addEventListener("DOMContentLoaded", () => {
+      updatePlayerText();
+    })
     // for each field add a eventlistener and print out the square
     fields.forEach((field) => field.addEventListener("click", (e) =>{
-
       // the position to change
       let position = e.target.dataset.num;
-
+      let player = gameControl.getCurrentPlayer();
       // if the spot is empty play the move
       if (e.target.innerHTML === ""){
-        updatePlayerText();
+
         // the sign to be puted in
         gameControl.playRound(position);
         // Change the board and update it
-        gameBoard.changeBoard(gameControl.getCurrentPlayer(), position);
+        gameBoard.changeBoard(player.getSign(), position);
         updateBoard();
+        updatePlayerText();
       }
     }))
+
+    reset_button.addEventListener("click", () =>{
+      gameBoard.resetBoard()
+      gameControl.changeRound(1)
+      updatePlayerText();
+    })
+
 
     // Update the board display
     const updateBoard = () => {
@@ -72,10 +87,14 @@ const displayControl = (() => {
          }
       }
     } 
+
     // Update the text
     const updatePlayerText = () => {
-      player_text.innerHTML = `Player ${gameControl.getCurrentPlayer()} is playing`
+      let player = gameControl.getCurrentPlayer();
+      player_text.innerHTML = `Player ${player.getName()} is playing`
     }
+
+
 
     return{updateBoard}
 
@@ -86,40 +105,36 @@ const gameControl = (() => {
 
   // X fängt immer an 
 
-  const player1 = Player("aleks", "X");
+  const player1 = Player("aco", "X");
   const player2 = Player("jelena", "O");
-  let round = 1;
+  let round = 0;
 
 
   const playRound = (index_of_sign) =>{
     checkIfTie();
-    // console.log(getCurrentPlayer())
     round++
-    
+    console.log(round)
   }
 
-
+  // get the current player depening on the round that is being played
   const getCurrentPlayer = () => {
-    let current = round % 2 === 1 ? player1.getSign() : player2.getSign();
+    current = round % 2 === 0 ? player1 : player2;
     return current
   }
 
   const checkIfTie  = () => {
-    if (round === 10){
-      console.log("Finished")
-      // do whatever
+    if (round === 9){
       round = 1;
-      gameBoard.resetBoard();
     }
   }
 
+  const changeRound = (num) => round = num; 
 
   const checkifWinner = () => {
     // check if 3 in a row
   }
 
 
-
-  return {playRound, getCurrentPlayer}
+  return {playRound, getCurrentPlayer, changeRound}
 
 })();
